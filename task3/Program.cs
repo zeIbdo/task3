acc513c9b19f58c27cfc24ddcdb0764fc4f3d6c4
zeIbdo <ibrahimrm@code.edu.az>
2024-05-22 18:00:37 +0400

    Reverse array was completed

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            #region ters array
+            int[] numbers = { 2, 5, 8, 9 };
+            int temp = 0;
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[numbers.Length - 1 - i];
+                numbers[numbers.Length - 1 - i] = temp;
+
+
+            }
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine(numbers[j]);
+            }
+            #endregion
         }
     }
 }
